2661824ded6a29743346bc6569407df54758c7ab
Marcos Eduardo <marcos@nouva.se>
2023-02-10 14:09:09 -0300

    Consumer lambda version

--- Consumer/src/Application/Program.java
+++ Consumer/src/Application/Program.java
@@ -6,6 +6,7 @@ import Util.PriceUpdate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 public class Program {
     public static void main(String[] args) {
@@ -17,6 +18,10 @@ public class Program {
         list.add(new Product("Mouse", 50.00));
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
+
+        Consumer<Product> cons = p -> {
+          p.setPrice(p.getPrice() * 1.1);
+        };
 
         list.forEach(Product::nonStaticPriceUpdate);
 
